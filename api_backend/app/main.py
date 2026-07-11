80788d19807e719c77ac397f6d2a68465c3c7d6b
AyoIbitola <mremmatola@gmail.com>
2026-07-11 07:42:48 +0100

    added CORS implementation

--- api_backend/app/main.py
+++ api_backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.auth.router import router as auth_router
 from app.products.router import router as products_router
@@ -6,6 +7,14 @@ from app.search.router import router as search_router
 from app.slack.router import router as slack_router
 
 app = FastAPI(title="Office Hardware Inventory API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(products_router)
